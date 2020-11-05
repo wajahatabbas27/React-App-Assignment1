42ade7fbef8d8c6aab182a4089b96696ddec57e7
wajahataz <54248569+wajahatabbas27@users.noreply.github.com>
2020-11-05 07:05:59 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './Hello.js';
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <strong>HEllo Wajahat Abbas</strong>
+      <br/>
+      <p>There are 5 subjects in {props.name}</p>
+      <ol>
+        <li>Maths</li>
+        <li>Sst</li>
+        <li>Science</li>
+        <li>English</li>
+        <li>Urdu</li>
+      </ol>
+      <Hello name="SWAZ"></Hello>
     </div>
   );
 }
---
+++ src/Hello.js
@@ -0,0 +1,12 @@
+import './Hello.css';
+
+function Hello({name}){
+    return (
+    <div className="myname">
+        This is Hello component by me.! {name}.
+
+    </div>
+    );
+}
+
+export default Hello;
